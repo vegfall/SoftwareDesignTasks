d817ca6ee6f2e7de4e7a246779297a7d03695b9b
Vegard Falldalen <vegfall@gmail.com>
2023-10-23 12:16:41 +0200

    Task 2 complete

--- Program.cs
+++ Program.cs
@@ -6,7 +6,11 @@
         {
             //_ = new Task1_1();
             //_ = new Task1_2();
-            _ = new Task1_3();
+            //_ = new Task1_3();
+
+            //_ = new Task2_1();
+            //_ = new Task2_2();
+            _ = new Task2_3();
 
             Console.ReadKey();
         }
@@ -119,5 +123,105 @@
                 Console.WriteLine($"{person[0]} and {person[1]} are of the same age ({age[0]}).");
             }
         }
+    }
+
+    class Task2_1
+    {
+        public Task2_1()
+        {
+            Start();
+        }
+
+        private static void Start()
+        {
+            int[] numbers = new int[9];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = i + 1;
+            }
+
+            Console.WriteLine("---Verion 1---");
+            for (int i = numbers.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+
+            Console.WriteLine("---Version 2---");
+            Array.Reverse(numbers);
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+        }
+    }
+
+    class Task2_2
+    {
+        public Task2_2()
+        {
+            Start();
+        }
+
+        private static void Start()
+        {
+            int[] dice;
+            int numberOfDice = 0;
+
+            Console.WriteLine("How many dice would you like to roll?");
+            numberOfDice = int.Parse(Console.ReadLine());
+
+            dice = new int[numberOfDice];
+
+            for (int i = 0; i < dice.Length; i++)
+            {
+                dice[i] = randomNumber(1, 6);
+
+                Console.WriteLine($"Dice #{i + 1} is {dice[i]}");
+            }
+
+            Console.WriteLine($"The sum of all dice is: {dice.Sum()}");
+        }
+
+        private static int randomNumber(int low, int high)
+        {
+            Random random = new Random();
+
+            return random.Next(low, high + 1);
+        }
+    }
+
+    class Task2_3
+    {
+        public Task2_3()
+        {
+            Start();
+        }
+
+        private static void Start()
+        {
+            List<int> fibbonaciList = new();
+            fibbonaciList.Add(0);
+            fibbonaciList.Add(1);
+
+            Console.WriteLine("How many numbers of the Fibonacci sequence would you like to calculate?");
+
+            int numberOfValues = int.Parse(Console.ReadLine());
+
+            for (int i = 2; i < numberOfValues; i++)
+            {
+                fibbonaciList.Add(
+                    fibbonaciList[fibbonaciList.Count - 1] +
+                    fibbonaciList[fibbonaciList.Count - 2]);
+            }
+
+            for (int i = 0; i < numberOfValues; i++)
+            {
+                Console.WriteLine($"| {fibbonaciList[i]} |");
+            }
+        }
+
+
     }
 }
